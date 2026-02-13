7da55eac8f8260ce3e1447379ec9f5a2602be7b1
Erick Solorzano <ericksolorzanolopez@gmail.com>
2026-02-12 19:30:53 -0600

    cambio del readme y el index

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # test1
 repositorio para mostrar mis skills trabajando con git
+
+## Institucion educativa
+Universidad Tecnologica de la Selva
+
+## Lugar de procedencia
+Ocosingo, Chiapas. 2026
---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Erick's Page</title>
+</head>
+<body>
+    <h1>
+        Alooo a ala pagina de Erick :D
+    </h1>
+</body>
+</html>
